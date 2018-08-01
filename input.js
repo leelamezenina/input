eeff8810c4977339e5f3f5b4362a6cb70d5ffa74
Leela Mezenina <leelamez@gmail.com>
2018-08-01 17:47:49 +0200

    Made site

--- input.js
+++ input.js
@@ -1,8 +1,6 @@
 document.getElementById('but').addEventListener ("click", function(){
     var fname = document.getElementById('name').value;
     var surname = document.getElementById('surname').value;
-    var female = document.getElementById('female').value;
-    var male = document.getElementById('male').value;
     var gender = document.getElementById('gender').value;
-   document.getElementById("result").innerHTML = fname + ' ' + surname + ' ' +  gender;
+          document.getElementById("result").innerHTML = fname + ' ' + surname + ' ' +  (gender === 'male' ? "Выиграл" : "Выиграла"); 
    });
